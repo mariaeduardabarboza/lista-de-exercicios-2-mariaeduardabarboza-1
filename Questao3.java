94af70705509d30f16909393279499bebd2f4aa5
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-08-27 14:17:15 -0300

    Questão3.java
    Resolução: O erro é o uso do Math.pow com o uso de (1/2) no programa, sua problemática foi resolvida pelo uso do Math.sqrt.

--- Questao3.java
+++ Questao3.java
@@ -5,7 +5,7 @@ public class Questao3
 	public static void main(String[] args) {
 	Scanner teclado = new Scanner(System.in); 
 	double x1, y1, x2, y2, d;
-	System.out.println("Entre com as coordenadas x e y dos pontos nesta ordem:");
+	System.out.println("Digite as coordenadas x e y dos pontos, respectivamente:");
 	x1 = teclado.nextFloat();
 	y1 = teclado.nextFloat(); 
 	x2 = teclado.nextFloat(); 
