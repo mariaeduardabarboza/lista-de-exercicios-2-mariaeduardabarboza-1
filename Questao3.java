dcf06f74a173a08951b2de43c43c0011bbc68021
Maria Eduarda Barboza Santos <88686480+mariaeduardabarboza@users.noreply.github.com>
2021-08-27 14:14:01 -0300

    Update Questao3.java

--- Questao3.java
+++ Questao3.java
@@ -4,14 +4,14 @@ public class Questao3
 {
 	public static void main(String[] args) {
 	Scanner teclado = new Scanner(System.in); 
-	double x1, y1, x2, y2, distancia;
+	double x1, y1, x2, y2, d;
 	System.out.println("Entre com as coordenadas x e y dos pontos nesta ordem:");
 	x1 = teclado.nextFloat();
 	y1 = teclado.nextFloat(); 
 	x2 = teclado.nextFloat(); 
 	y2 = teclado.nextFloat(); 
-	distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); 
-	System.out.println("A distância é: " + distancia);
+	d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); 
+	System.out.println("A distância é: " + d);
 	    
 	} 
     
